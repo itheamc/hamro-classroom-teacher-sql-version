a64df4b0b35c70d928ebb302f64a5d2784802c97
Amit Chaudhary <itheamc@gmail.com>
2021-08-28 22:30:33 +0545

    Version 1.0.0 - Beta (Add Notice Function Added and Minor Bug Fixed)

--- app/src/main/java/com/itheamc/hamroclassroom_teachers/ui/AddNoticeFragment.java
+++ app/src/main/java/com/itheamc/hamroclassroom_teachers/ui/AddNoticeFragment.java
@@ -2,16 +2,67 @@ package com.itheamc.hamroclassroom_teachers.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.itheamc.hamroclassroom_teachers.R;
+import com.itheamc.hamroclassroom_teachers.callbacks.QueryCallbacks;
+import com.itheamc.hamroclassroom_teachers.databinding.FragmentAddNoticeBinding;
+import com.itheamc.hamroclassroom_teachers.handlers.QueryHandler;
+import com.itheamc.hamroclassroom_teachers.models.Assignment;
+import com.itheamc.hamroclassroom_teachers.models.Material;
+import com.itheamc.hamroclassroom_teachers.models.Notice;
+import com.itheamc.hamroclassroom_teachers.models.School;
+import com.itheamc.hamroclassroom_teachers.models.Student;
+import com.itheamc.hamroclassroom_teachers.models.Subject;
+import com.itheamc.hamroclassroom_teachers.models.Submission;
+import com.itheamc.hamroclassroom_teachers.models.User;
+import com.itheamc.hamroclassroom_teachers.utils.ArrayUtils;
+import com.itheamc.hamroclassroom_teachers.utils.IdGenerator;
+import com.itheamc.hamroclassroom_teachers.utils.LocalStorage;
+import com.itheamc.hamroclassroom_teachers.utils.NotifyUtils;
+import com.itheamc.hamroclassroom_teachers.utils.TimeUtils;
+import com.itheamc.hamroclassroom_teachers.utils.ViewUtils;
+import com.itheamc.hamroclassroom_teachers.viewmodels.MainViewModel;
 
-public class AddNoticeFragment extends Fragment {
+import java.util.List;
 
+public class AddNoticeFragment extends Fragment implements QueryCallbacks {
+    private static final String TAG = "AddNoticeFragment";
+    private FragmentAddNoticeBinding addNoticeBinding;
+    private NavController navController;
+    private MainViewModel viewModel;
+
+    /*
+    TextInputLayouts
+     */
+    private TextInputLayout titleTextInputLayout;
+    private TextInputLayout descTextInputLayout;
+    private TextInputLayout classesTextInputLayout;
+
+    /*
+    EditTexts
+     */
+    private EditText titleEditText;
+    private EditText descEditText;
+    private EditText classesEditText;
+
+    /*
+    Strings
+     */
+    String title = null;
+    String desc = null;
+    String[] classes = null;
 
     public AddNoticeFragment() {
         // Required empty public constructor
@@ -25,9 +76,147 @@ public class AddNoticeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_notice, container, false);
+        addNoticeBinding = FragmentAddNoticeBinding.inflate(inflater, container, false);
+        return addNoticeBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initializing NavController and MainViewModel
+        navController = Navigation.findNavController(view);
+        viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+
+        // Initializing TextInputLayouts and EditTexts
+        titleTextInputLayout = addNoticeBinding.titleInputLayout;
+        descTextInputLayout = addNoticeBinding.descInputLayout;
+        classesTextInputLayout = addNoticeBinding.classesInputLayout;
+
+        titleEditText = titleTextInputLayout.getEditText();
+        descEditText = descTextInputLayout.getEditText();
+        classesEditText = classesTextInputLayout.getEditText();
+
+
+    }
+
+    /*
+    Function to add notice
+     */
+    private void addNotice() {
+        if (!isInputsValid()) return;
+
+        User user = viewModel.getUser();
+        if (user == null) {
+            if (getActivity() != null) {
+                QueryHandler.getInstance(this).getUser(LocalStorage.getInstance(getActivity()).getUserId());
+                showProgress();
+            }
+            return;
+        }
+
+        Notice notice = new Notice(
+                IdGenerator.generateRandomId(),
+                title,
+                desc,
+                user.get_schools_ref(),
+                null,
+                classes,
+                user.get_id(),
+                null,
+                TimeUtils.now()
+        );
+
+        QueryHandler.getInstance(this).addNotice(notice);
+        showProgress();
+    }
+
+
+    /*
+    Function to check if inputs are valid
+     */
+    private boolean isInputsValid() {
+        if (titleEditText != null) title = titleEditText.getText().toString().trim();
+        if (descEditText != null) desc = descEditText.getText().toString().trim();
+        if (classesEditText != null) {
+            if (!classesEditText.getText().toString().trim().isEmpty()) classes = ArrayUtils.toArray(classesEditText.getText().toString().trim(), ", ");
+        }
+
+        if ((title == null || title.isEmpty()) && (desc == null || desc.isEmpty()) && (classes == null || classes.length == 0)) {
+            titleTextInputLayout.setError("Please input title");
+            descTextInputLayout.setError("Please input description");
+            classesTextInputLayout.setError("Please input classes");
+            return false;
+        }
+
+        if (title == null || title.isEmpty()) {
+            titleTextInputLayout.setError("Please input title");
+            return false;
+        }
+
+        if (desc == null || desc.isEmpty()) {
+            descTextInputLayout.setError("Please input description");
+            return false;
+        }
+
+        if (classes == null || classes.length == 0) {
+            classesTextInputLayout.setError("Please input classes");
+            return false;
+        }
+
+        return true;
+    }
+
+    /*
+    Function to show progress
+     */
+    private void showProgress() {
+        ViewUtils.showProgressBar(addNoticeBinding.progressBarContainer);
+        ViewUtils.disableViews(addNoticeBinding.addNoticeButton, titleTextInputLayout, descTextInputLayout);
+    }
+
+    /*
+    Function to hide progress
+     */
+    private void hideProgress() {
+        ViewUtils.hideProgressBar(addNoticeBinding.progressBarContainer);
+        ViewUtils.enableViews(addNoticeBinding.addNoticeButton, titleTextInputLayout, descTextInputLayout);
+    }
+
+    /**
+     * -----------------------------------------------------------------------------------------
+     * Methods implemented from the QueryCallbacks
+     */
+    @Override
+    public void onQuerySuccess(List<User> user, List<School> schools, List<Student> students, List<Subject> subjects, List<Assignment> assignments, List<Material> materials, List<Submission> submissions, List<Notice> notices) {
+
+    }
+
+    @Override
+    public void onQuerySuccess(User user, School school, Student student, Subject subject, Assignment assignment, Material material, Submission submission, Notice notice) {
+        if (addNoticeBinding == null) return;
+        if (user != null) {
+            viewModel.setUser(user);
+            addNotice();
+        }
+    }
+
+    @Override
+    public void onQuerySuccess(String message) {
+        if (addNoticeBinding == null) return;
+        hideProgress();
+        if (getContext() != null && message.equals("success")) NotifyUtils.showToast(getContext(), "Added Successfully");
+        else NotifyUtils.showToast(getContext(), message);
+    }
+
+    @Override
+    public void onQueryFailure(Exception e) {
+        if (addNoticeBinding == null) return;
+        hideProgress();
+        if (getContext() != null) NotifyUtils.showToast(getContext(), "Unable to add");
+        NotifyUtils.logError(TAG, "onFailure()", e);
     }
 }
